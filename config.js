1cc90b381970a98467b3462ab3eceeb4a76bdafe
Aditya Y <aditya@netroy.in>
2011-12-30 05:47:01 +0530

    updated config

--- config.js
+++ config.js
@@ -4,16 +4,12 @@ var config = {
   },
   irc: {
     server: "irc.freenode.net",
-    channels: ["#Node.js", "##javascript", "#jquery", "#html5", "#css", "#nginx", "#redis", "#git"],
-    nick: "Badababuba",
-    name: "BadaBabuBa",
+    channels: ["#hasgeek", "#Node.js", "##javascript", "#jquery", "#html5", "#css", "#nginx", "#redis", "#git"],
+    nick: "QiBala",
+    name: "QiBala",
     pass: null,
     awayMsg: "gone away"
   },
-  twitter: {
-    key: "QwG0YGi02cJBl59Uv7w3jg",
-    secret: "vN0Ry5ZGccyazCbLJ5dhb8vicH6oe4x5KoEN99O7DY"
-  },
-  MAX_LOG: 200
+  MAX_LOG: 100
 };
 exports.config = config;
