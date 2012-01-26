05de6ef6b1b9f67f1adb2f5ea0aa75b9b388e12f
Aditya Y <aditya@netroy.in>
2012-01-27 01:51:12 +0530

    message sending moved into a separate module

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 // Imports
 var    fs = require('fs'),
-       io = require("socket.io"),
+   config = require("./config"),
   express = require('express'),
   connect = require('connect'),
    stylus = require("stylus"),
-   events = require('events'),
 everyauth = require('everyauth'),
-      app = express.createServer(),
-   config = require("./config");
+      app = express.createServer();
 
 app.configure(function() {
   app.set('views', __dirname + '/views');
@@ -79,76 +77,13 @@ app.get("/old", function(req,resp){
 
 // Start listening
 if (!module.parent) {
-  io = io.listen(app);
   app.listen(process.env['app_port'] || config.http.port);
   console.info("server started at http://localhost:%d/", app.address().port);
 }
 
 var backlog = {};
-
-// Initialize the message queue for posting
-var mQueue = new events.EventEmitter();
-
-// Bind Socket.IO server to the http server
+var io = require("socket.io");
+io = io.listen(app);
 io.set('log level', 2);
-io.sockets.on('connection', function(client){
-  mQueue.emit('connection', null);
-
-  for(var channel in backlog) {
-    if(channel === 'server') {
-      client.json.send({'motd': backlog["server"]});
-    } else {
-      client.json.send({'channel':channel, topic:backlog[channel]["topic"]});
-      client.json.send({'channel':channel, names:backlog[channel]["names"]});
-      client.json.send({'message': backlog[channel]["messages"]});
-    }
-  }
-
-  client.json.send({'backlog': true});
-
-  client.on('message', function(message){
-    mQueue.emit('message', message);
-  });
-
-  client.on('disconnect',function(){
-    mQueue.emit('disconnect', null);
-  });
-});
-
 var ircClient = require("./lib/ircClient").init(config.irc, io, backlog);
-
-// For sending push to message queue
-var msgObject, clientCount = 0, awayTimer;
-mQueue.addListener('message', function(message){
-  if(!!backlog[message.channel] && ircClient.conn.writable === true){
-    ircClient.say(message.channel, message.text);
-    msgObject = {
-      from: config.irc.nick,
-      channel: message.channel,
-      text: message.text,
-      time: (new Date()).toUTCString()
-    };
-    backlog[message.channel]["messages"].push(msgObject);
-    io.sockets.json.send({'echo':msgObject});
-  }
-});
-
-mQueue.addListener('connection', function(){
-  clearTimeout(awayTimer);
-  if(ircClient.conn.writable !== true) return;
-  if(++clientCount === 1){
-    awayTimer = setTimeout(function(){
-      ircClient.away();
-    },15000);
-  }
-});
-
-mQueue.addListener('disconnect', function(){
-  clearTimeout(awayTimer);
-  if(ircClient.conn.writable !== true) return;
-  if(--clientCount < 1){
-    awayTimer = setTimeout(function(){
-      ircClient.away(config.irc.awayMsg);
-    },15000);
-  }
-});
+var mQueue = require("./lib/messageQueue").init(config.irc, io, backlog, ircClient);
